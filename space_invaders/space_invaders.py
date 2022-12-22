daa1c19c338652eb7c1218e7591cb47decf564d6
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2022-12-22 03:09:31 -0600

    working space invaders

--- space_invaders/space_invaders.py
+++ space_invaders/space_invaders.py
@@ -19,7 +19,7 @@ pygame.display.set_caption("Space Invaders")
 background = pygame.image.load("bg.webp")
 
 # player
-playerImg = pygame.image.load('player64px.png')
+playerImg = pygame.image.load('player_retro_blue64px.png')
 playerX = 370
 playerY = 480
 playerX_change = 0
@@ -37,7 +37,7 @@ for i in range(num_of_enemies):
     enemyImg.append(pygame.image.load('enemy.png'))
     enemyX.append(random.randint(0, 735))
     enemyY.append(random.randint(50, 150))
-    enemyX_change.append(0.3)
+    enemyX_change.append(.5)
     enemyY_change.append(40)
 
 # laser
@@ -46,7 +46,7 @@ laserX = []
 laserY = []
 laserY_change = []
 laser_state = []
-max_lasers = 3
+max_lasers = 7
 for i in range(max_lasers):
     laserY.append(480)
     laserImg.append(pygame.image.load('laser.png'))
@@ -60,9 +60,16 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 textX = 10
 textY = 10
 
+# Game over font
+over_font = pygame.font.Font('freesansbold.ttf', 72)
+
 def show_score(x,y):
     score = font.render("Score: " + str(score_value), True, (255, 255, 255))
     screen.blit(score, (x, y))
+
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(over_text, (182, 250))
 
 def enemy(x, y, i):
     screen.blit(enemyImg[i], (x, y))
@@ -116,6 +123,11 @@ while running:
 
     # Enemy Movement
     for i in range(num_of_enemies):
+        if enemyY[i] > 440:
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000
+            game_over_text()
+            break
         enemyX[i]+= enemyX_change[i]
         if enemyX[i] <= 0:
             enemyX_change[i] = 0.3
